b4c8948e7a209fbafe71a34a43b8251cca5a7f82
Vigaash-Lingeswaran <vigaash29@gmail.com>
2026-07-08 14:41:36 -0400

    made a new background and edited window
    Made a better background (clearer), and also started creating the style for other windows, and finished up the first window's close feature

--- script.js
+++ script.js
@@ -5,3 +5,77 @@ function updatetime()
             timeText.innerHTML = currentTime;
         }
     setInterval(updatetime, 1000);
+
+dragElement(document.getElementById("Welcome"));
+
+function dragElement(element) {
+  
+  var initialX = 0;
+  var initialY = 0;
+  var currentX = 0;
+  var currentY = 0;
+
+ 
+  if (document.getElementById(element.id + "header")) {
+    
+    document.getElementById(element.id + "header").onmousedown = startDragging;
+  } else {
+    element.onmousedown = startDragging;
+  }
+
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+    initialX = e.clientX;
+    initialY = e.clientY;
+    document.onmouseup = stopDragging;
+    document.onmousemove = dragElement;
+  }
+
+  function dragElement(e) {
+    e = e || window.event;
+    e.preventDefault();
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+var welcomeScreen = document.querySelector("#Welcome")
+var welcomescreenclose = document.querySelector("#Welcomeclose")
+var welcomescreenopen= document.querySelector("#Welcomeopen")
+
+
+function closewindow(element)
+{
+    element.style.display = "none"
+}
+
+function openwindow (element)
+{
+    element.style.display = "flex"
+}
+
+welcomescreenclose.addEventListener("click",
+
+function ()
+{
+    closewindow(welcomeScreen);
+}
+);
+
+welcomescreenopen.addEventListener("click",
+
+function ()
+{
+    openwindow(welcomeScreen);
+}
+);
